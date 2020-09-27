96b8d57807ec6f1f2ac78eed90bf6825704c1f40
jayantsaraf <sarafjayant24@gmail.com>
2020-09-27 15:28:36 +0530

    [jayant] Add. Added welcome message

---
+++ LineComparision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wlcome to Line Comparison Program!");
+        }
+    }
+}
